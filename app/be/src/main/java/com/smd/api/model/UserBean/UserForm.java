4eb008125522a87be3a363e0ceef29446138b064
kitutune <p.kitutune@gmail.com>
2022-08-06 15:30:43 +0900

    アノテーションの修正

--- app/be/src/main/java/com/smd/api/model/UserBean/UserForm.java
+++ app/be/src/main/java/com/smd/api/model/UserBean/UserForm.java
@@ -1,4 +1,8 @@
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -13,6 +17,8 @@ import lombok.Setter;
 @Setter
 public class UserForm {
     @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotNull
     private String name;
