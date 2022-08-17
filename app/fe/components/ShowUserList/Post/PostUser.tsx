991e2d2e839d1d6d8839ab432d166458f365a99e
kitutune <p.kitutune@gmail.com>
2022-08-17 22:07:41 +0900

    デリートの機能の実装を補完

--- app/fe/components/ShowUserList/Post/PostUser.tsx
+++ app/fe/components/ShowUserList/Post/PostUser.tsx
@@ -1,1 +1,32 @@
-export const usePostUser = () => {};
+import axios from "axios";
+import { useCallback, useEffect } from "react";
+
+export const usePostUser = (props) => {
+  console.log(props);
+
+  const BASEURL = "http://localhost:8080/api/users";
+
+  const dbRegistered = useCallback((formUser) => {
+    axios
+      .post(BASEURL, formUser, {
+        // デフォルト値がapplication/jsonなので記述必要なし
+        // headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => {
+        if (response.status === 200) console.log("登録成功");
+
+        console.log(response.status);
+        console.log(response.data);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   if (formUser.name === "" || formUser.email === "") {
+  //     return console.log("空の値は登録できません");
+  //   }
+  //   console.log("登録します");
+  //   dbRegistered(formUser);
+  // }, [formUser]);
+
+  return dbRegistered;
+};
--- app/fe/components/ShowUserList/ShowUserList.tsx
+++ app/fe/components/ShowUserList/ShowUserList.tsx
@@ -2,57 +2,50 @@ import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 import { useGetUserList } from "./Get/GetUserList";
 import { UsersList } from "./UserList";
+const swichDisplay = (userList) => {
+  switch (userList) {
+    // case userList.length === 0:
+    case true:
+      console.log("karakarakara");
+
+      return <>ユーザーリストは空です</>;
+
+    default:
+      return (
+        <>
+          <UsersList data={userList} />
+        </>
+      );
+  }
+};
 
 export const ShowUserList = () => {
   const userList = useGetUserList();
-  // const BASEURL = "http://localhost:8080/api/users";
-  // // const BASEURL = "localhost:8080/api/test";
-  // const [userList, setUserList] = useState([
-  //   {
-  //     id: "",
-  //     name: "",
-  //     age: "",
-  //     email: "",
-  //     todo: "",
-  //   },
-  // ]);
-  // // userテーブルから取得
-  // const getUserList = useCallback(async () => {
-  //   const response = await axios.get(BASEURL).catch((error) => {
-  //     // レスポンスありのエラーハンドリング（実際には必要に応じた例外処理を実装する）
-  //     console.log(
-  //       `Error! code: ${error.response.status}, message: ${error.message}`
-  //     );
-  //     // return error.response;
-  //   });
-  //   // return response.data;
-  //   setUserList(response.data);
-  // }, []);
-
-  // // // 実行結果
-  // // getUserList().then((userlist) => {
-  // //   setUserList(userlist);
-
-  // //   console.log(userlist.length);
-  // // });
-  // console.log("レンダリング");
-  // console.log(userList);
-  // console.log(userList.length);
-  // console.log(userList[0].id);
-  // console.log(userList[0].id === "");
-
-  // if (!(userList[0].id === "")) {
-  //   console.log("変換", Object.entries(userList));
-  // }
+  console.log(userList);
+  console.log(userList === []);
+  console.log(!(userList === []));
+  console.log(userList.length);
 
   // useEffect(() => {
-  //   getUserList();
-  // }, []);
+  //   if(userList===[]){
+  //     return <>ユーザーリストは空です</>
+  //   }
+  // }, [userList])
+
   return (
     <>
       {/* <button onClick={getUserList()}>ゆーざーりすと</button> */}
-      <div>ShowUserList</div>
-      {!(userList[0].id === "") ? <UsersList data={userList} /> : null}
+      {/* <div>ShowUserList</div>
+      {!(userList[0].id === "") ? <UsersList data={userList} /> : null} */}
+      {/* {swichDisplay(userList)} */}
+      {console.log(UsersList)}
+      {console.log(UsersList.length)}
+      {console.log(UsersList.length === 0)}
+      {UsersList.length > 0 ? (
+        <UsersList data={userList} />
+        ) : (
+        <>ユーザーリストは空です</>
+      )}
     </>
   );
 };
